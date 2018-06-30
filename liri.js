874d9515411b80c4e8f085e24796b8ea44bbe93b
Missy Norman <missy.norman412@gmail.com>
2018-06-30 12:19:05 -0400

    adding

--- liri.js
+++ liri.js
@@ -21,8 +21,8 @@ switch (action) {
             var movieEntry = "Mr. Nobody";
         }
         //console.log(movieEntry);
-       movie(movieEntry);
-               break;
+        movie(movieEntry);
+        break;
 
 }
 
@@ -40,15 +40,14 @@ function printTweets() {
 }
 
 function spotifySong(querySong) {
-        //console.log(querySong);
+    //console.log(querySong);
     spotify.search({ type: 'track', query: querySong }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-            console.log(data[0]);
+        console.log(data);
     });
 }
-
 function movie(movieEntry) {
     if (movieEntry === "undefined") {
         movieEntry = "Mr. Nobody";
@@ -56,19 +55,33 @@ function movie(movieEntry) {
     var request = require("request");
     // Then run a request to the OMDB API with the movie specified
     console.log(movieEntry);
-    request("http://www.omdbapi.com/?t=" + movieEntry, function (error, response, body) {
-        
-    
+
+    // Then run a request to the OMDB API with the movie specified
+    request("http://www.omdbapi.com/?t=" + movieEntry + "&apikey=trilogy", function (error, response, body) {
         // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
-            console.log("here");  
+            console.log("here");
             // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            
+            console.log("The movie's title is: " + JSON.stringify(JSON.parse(body),null,2));
+            
+             console.log("The movie's title is: " + JSON.parse(body).Title);
+             console.log("The year the movie came out is: " + JSON.parse(body).Year);
+             console.log("The movie's imdb rating is: " + JSON.parse(body).imdbRating);
+            // var arrLength = JSON.parse(body).Ratings.length;
+            // for (i = 0; i < arrLength; i++){
+            //     if (JSON.parse(body).Ratings.Source === "Rotten Tomatoes"){
+            //         console.log("The Rotten Tomatoes value is: " + JSON.parse(body).Ratings[Source])
+            //     }
+            // }
+            // console.log(arrLength);
+            //console.log("The movie's Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[Source]['Rotten Tomatoes']);
 
 
-        }  
+        }
+
+
     });
-    
 
 }
+
